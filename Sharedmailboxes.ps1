08db8b44be17723eb441f9b39b8b08c9ab629f34
Yevhenii Vasiutenko <the.geka@gmail.com>
2022-01-06 17:25:52 +0000

    Update Sharedmailboxes.ps1

--- Sharedmailboxes.ps1
+++ Sharedmailboxes.ps1
@@ -1,4 +1,4 @@
-Connect-ExchangeOnline
+#Connect-ExchangeOnline
 Get-Mailbox -Filter {recipienttypedetails -eq "SharedMailbox"} | Select-Object ExternalDirectoryObjectId| ForEach-Object { 
         Get-MailboxPermission $_.ExternalDirectoryObjectId
-    } | Select-Object Identity, User | ft -Wrap -AutoSize | Out-File .\exportsharedmb.txt
+    } | Select-Object Identity, User, AccessRights | ft -Wrap -AutoSize | Out-File .\exportsharedmb.txt
